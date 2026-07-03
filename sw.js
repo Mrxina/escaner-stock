0d8ab5ba64f3cd419ffe0999fc2aa96b5b0dbd9e
Mrxina <mrxina@gmail.com>
2026-07-03 07:38:33 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+/* Escáner de Stock · Service Worker (funcionamiento sin conexión) */
+const CACHE = 'escaner-stock-v1.0.0';
+const FILES = [
+  './',
+  './index.html',
+  './zxing.min.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request, { ignoreSearch: true }).then((r) => r || fetch(e.request))
+  );
+});
